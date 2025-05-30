6e36f522fb5c0512c1ba8d9f8d2e1bbec6dbfe86
Devivo-dev <devivo.dev@gmail.com>
2025-05-30 20:52:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  root: './',
+  root: 'src',
   build: {
     rollupOptions: {
       input: glob.sync('./src/*.html'),
