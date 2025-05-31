0ffa5c6e860df3cbb75f8cd490f9bfabb36e0b1b
Devivo-dev <devivo.dev@gmail.com>
2025-05-31 11:47:42 +0200

    Update onGameOver.js

--- src/scripts/game/onGameOver.js
+++ src/scripts/game/onGameOver.js
@@ -1,6 +1,6 @@
 import { updateWhiskas } from './whiskas.js'
 
-export function onGameOver(scoreAmount) {
+export async function onGameOver(scoreAmount) {
   const coinsCollected = scoreAmount;
   localStorage.setItem('coins_collected', coinsCollected.toString());
   await updateWhiskas('test-player-001', coinsCollected);
