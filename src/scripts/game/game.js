7b1d95e8ff0b454f3e26565217e7cc0bd1594bad
Devivo-dev <devivo.dev@gmail.com>
2025-05-31 10:45:52 +0200

    Update game.js

--- src/scripts/game/game.js
+++ src/scripts/game/game.js
@@ -3,6 +3,13 @@ import { startCountdown } from './ui.js';
 import { spawnCoin } from './coin.js';
 import { updateGame } from './engine.js';
 
+const coinsCollected = 25 // або змінна, яка підраховує
+localStorage.setItem('coins_collected', coinsCollected.toString())
+
+// Оновлення у базі
+import { updateWhiskas } from './whiskas.js'
+updateWhiskas('test-player-001', coinsCollected)
+
 export function initGame() {
   spawnCoin();
   startCountdown(() => updateGame());
