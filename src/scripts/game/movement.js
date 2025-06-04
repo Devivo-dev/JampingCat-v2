b7915ff028628688d8f1a2d7221d24cb3e292dd1
MrForestino <devivo.dev@gmail.com>
2025-06-04 23:46:35 +0200

    solve

--- src/scripts/game/movement.js
+++ src/scripts/game/movement.js
@@ -1,10 +1,25 @@
 //логіка руху персонажа
 import { state } from './state.js';
 
+function getAdaptiveSpeed() {
+  const width = window.innerWidth;
+  if (width < 400) return 2;       // телефони
+  if (width < 768) return 5;       // планшети
+  return 4;                        // великі монітори
+}
+function getAdaptiveHeight() {
+  const width = window.innerWidth;
+  if (width < 400) return 7;       // телефони
+  if (width < 768) return 10;       // планшети
+  return 10;                        // великі монітори
+}
+const speed = getAdaptiveSpeed();
+const height = getAdaptiveHeight();
+
 export function moveLeft() {
   if (!state.running) return;
-  state.velocityY = -8;
-  state.velocityX = -1;
+  state.velocityY = -height;
+  state.velocityX = -speed;
   state.score++;
   document.querySelector(".control__button--left").classList.add("active");
   setTimeout(() => {
@@ -15,8 +30,8 @@ export function moveLeft() {
 
 export function moveRight() {
   if (!state.running) return;
-  state.velocityY = -8;
-  state.velocityX = 1;
+  state.velocityY = -height;
+  state.velocityX = speed;
   state.score++;
   document.querySelector(".control__button--right").classList.add("active");
   setTimeout(() => {
--- src/scripts/game/onGameOver.js
+++ src/scripts/game/onGameOver.js
@@ -1,29 +1,13 @@
 import { updateWhiskas } from '../../whiskas.js';
 import { state } from './state.js';
 
-// 🔽 Функція — перед використанням
-function formatCoins(number) {
-  if (number >= 1_000_000_000) {
-    return (number / 1_000_000_000).toFixed(1) + 'B';
-  }
-  if (number >= 1_000_000) {
-    return (number / 1_000_000).toFixed(1) + 'M';
-  }
-  if (number >= 1_000) {
-    return (number / 1_000).toFixed(1) + 'k';
-  }
-  return number.toString();
-}
 
-document.getElementById('whiskas-amount').textContent = formatCoins(state.coins);
 
 export async function onGameOver() {
   const coinsCollected = state.coins;
 
   localStorage.setItem('whiskas-amount', coinsCollected.toString());
-  document.getElementById('whiskas-amount').textContent = formatCoins(coinsCollected);
 
   await updateWhiskas(coinsCollected); 
 }
-
 console.log('🟡 onGameOver() викликано');
